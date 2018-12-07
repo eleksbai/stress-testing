b23e74827642e7c69f9d0dff3d461ed3bfd03a56
hyman <eleksbai@gmail.com.work>
2018-12-07 11:40:36 +0800

    开多个爬虫速率,web.log显示速率翻倍(52>27)

--- test_web/test_web/spiders/simple.py
+++ test_web/test_web/spiders/simple.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+l = [None, None]
 
 class SimpleSpider(scrapy.Spider):
     name = 'simple'
@@ -8,5 +9,7 @@ class SimpleSpider(scrapy.Spider):
     start_urls = ['http://18.18.186.19:7000/']
 
     def parse(self, response):
-        yield scrapy.Request(url='http://18.18.186.19:7000/', callback=self.parse, dont_filter=True)
+        # 2的n次方
+        for i in l:
+            yield scrapy.Request(url='http://18.18.186.19:7000/', callback=self.parse, dont_filter=True)
 
